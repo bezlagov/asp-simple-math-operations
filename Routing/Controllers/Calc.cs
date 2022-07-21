5678ee6118507d2275aa98c2b2c173c22bc0325c
Ivan Zhyhalkin <ivan.zhigalkin@gmail.com>
2022-07-21 16:34:52 +0300

    Add project files.

---
+++ Routing/Controllers/Calc.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routing.Controllers
+{
+    public class Calc : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add(int x, int y)
+        {
+            return View("Result", (float)(x + y));
+        }
+
+        public IActionResult Mul(int x, int y)
+        {
+            return View("Result", (float)(x * y));
+        }
+
+        public IActionResult Div(int x, int y)
+        {
+            if (y==0)
+            {
+                return RedirectToAction("Error");
+            }
+                return View("Result", (float)(x / y));
+        }
+
+        public IActionResult Sub(int x, int y)
+        {
+            return View("Result", (float)(x - y));
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
